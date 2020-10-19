fbe9acad5d971e2fa880d00c32ab6284fda5041a
Anmeet <anmeet619@yahoo.com>
2020-10-19 16:14:06 +1100

    tdd-jotto-context

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {findByTestAttr} from '../test/testUtils';
+import App from './App';
+
+const setup = () => {
+  return shallow(<App />);
+}
+
+test('renders learn react link', () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, 'component-app');
+    expect(component.length).toBe(1);
+});
